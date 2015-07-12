83e10a79832288652c9937850b508c7212f66eff
blackfox1983 <blackfox1983@163.com>
2015-07-12 18:45:49 +0800

    add baidutongji

---
+++ static/baidutongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?48dffb76a7e3c22da512f0d15542b4b7";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
